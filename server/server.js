f2207e0e2cef85c076b2f38872c2e43e5d738710
Tanuja Saravale <saravaletanuja18@gmail.com>
2026-03-16 19:00:12 +0530

    authcontroller

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 // Test route
 app.get("/", (req, res) => {
