5fc7d48d409d8b30478fab0bd29550b3e39b850c
Suyash Patil <suyashpatil7574@gmail.com>
2026-03-17 20:06:23 +0530

    Completed backend APIs with auth and case management

--- server/server.js
+++ server/server.js
@@ -5,11 +5,9 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 
 dotenv.config();
-
 connectDB();
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
