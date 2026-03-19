be816cfdf6d29ff5ea4d252871c4c6a0b1f794ca
VIRAJ261-cloud <darkknightthe91@gmail.com>
2026-03-19 18:59:22 +0530

    auth connects with backend and lawyer's Client page creates

---
+++ client/src/services/authService.ts
@@ -0,0 +1,36 @@
+export  async function checkIt(email:string,password:string){
+    const res = await fetch("http://localhost:5000/api/auth/login",{
+        method:'POST',
+        headers:{
+        "Content-Type": "application/json",
+      },
+      body:JSON.stringify({
+        email,
+        password,
+      }),
+    });
+    const data = await res.json()
+    return {
+        ok:res.ok,
+        data,
+    }
+}
+export async function register(name:string,email:string,password:string,role:string) {
+    const res = await fetch("http://localhost:5000/api/auth/register",{
+        method:'POST',
+        headers:{
+            "Content-type":"application/json",
+        },
+        body:JSON.stringify({
+            name,
+            email,
+            password,
+            role,
+        })
+    })
+    const data = await res.json()
+    return {
+        ok:res.ok,
+        data,
+    }
+}
