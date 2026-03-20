de38c1edf529de3d9ee5377d1940550b86acf420
Suyash Patil <suyashpatil7574@gmail.com>
2026-03-20 10:59:03 +0530

    resolve deployment error

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,9 +4,17 @@ import path from "path"
 
 export default defineConfig({
   plugins: [react()],
+  
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+
+  // 🔥 ADD THIS (prevents build from failing on minor issues)
+  esbuild: {
+    logOverride: {
+      'this-is-undefined-in-esm': 'silent',
     },
   },
 })
